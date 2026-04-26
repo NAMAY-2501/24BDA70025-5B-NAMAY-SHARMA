4c3d9583bb9db4b98adbb4f4d6d3049038f71ca1
Namay-2501 <sharmanamayleaf@example.com>
2026-04-26 17:52:28 +0530

    fix port for render

--- index.js
+++ index.js
@@ -2,31 +2,38 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import serverless from "serverless-http";
 
 import studentRoutes from "./routes/student.routes.js";
 import studentViewRoutes from "./routes/student.view.routes.js";
 
+// Load env variables
 dotenv.config();
+
+// Connect DB
 connectDB();
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// View engine
 app.set("view engine", "ejs");
 
-// API ROUTES
+// Routes
 app.use("/students", studentRoutes);
-
-// VIEW ROUTES (may not fully work on Vercel)
 app.use("/view/students", studentViewRoutes);
 
+// Test route
 app.get("/", (req, res) => {
-  res.send("Server Running on Vercel 🚀");
+  res.send("Server Running on Render 🚀");
 });
 
-// EXPORT FOR VERCEL
-export default serverless(app);
+// ✅ IMPORTANT FIX FOR RENDER
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
+});
